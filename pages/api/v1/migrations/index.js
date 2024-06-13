4c15ee67c3fa0387c85617fef4b7f77a05bc2bd0
RaphaelPinheiro <162897423+RaphaelPinheiiro@users.noreply.github.com>
2024-06-13 10:03:30 -0300

    Create `/api/v1/migrations` endpoint

---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,37 @@
+import migrationsRunner from "node-pg-migrate";
+import { join } from "node:path";
+import database from "infra/database.js";
+
+export default async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
+
+  const defaultMigrationOptions = {
+    dbClient: dbClient,
+    dryRun: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+  if (request.method === "GET") {
+    const pendingMigrations = await migrationsRunner(defaultMigrationOptions);
+    await dbClient.end();
+    response.status(200).json([pendingMigrations]);
+  }
+
+  if (request.method === "POST") {
+    const migrateMigrations = await migrationsRunner({
+      ...defaultMigrationOptions,
+      dryRun: false,
+    });
+
+    await dbClient.end();
+
+    if (migrateMigrations.length > 0) {
+      return response.status(201).json(migrateMigrations);
+    }
+
+    return response.status(200).json([migrateMigrations]);
+  }
+  return response.status(405).end();
+}
